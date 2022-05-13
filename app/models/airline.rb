35aadc2156aa2f049be78d5d0f2dac84a4bea3e5
michelle <michellelohwt0799@gmail.com>
2022-05-13 19:01:14 +0800

    feat: add comments

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,9 +1,9 @@
 class Airline < ApplicationRecord
   has_many :reviews # An Airline has many Review
 
-  before_create :slugify
+  before_create :slugify # Slugify before saving new object in database
 
   def slugify
-    self.slug = name.parameterize
+    self.slug = name.parameterize # Create an human-readable unique identifier (eg: air-asia is better than id 0001)
   end
 end
