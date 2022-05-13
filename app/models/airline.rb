3e1a1754fc31e7760ebf4ad7c3b31d75e06ba4c8
michelle <michellelohwt0799@gmail.com>
2022-05-13 19:04:09 +0800

    feat: define function that calculate average score of reviews

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -6,4 +6,10 @@ class Airline < ApplicationRecord
   def slugify
     self.slug = name.parameterize # Create an human-readable unique identifier (eg: air-asia is better than id 0001)
   end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f # Calculate average score of the reviews, 
+                                            # round it to 2 decimal places, and
+                                            # change it to float type
+  end
 end
