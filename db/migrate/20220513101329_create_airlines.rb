6585f054bb1d2e5c160345d8eeb0bb4622867d46
michelle <michellelohwt0799@gmail.com>
2022-05-13 18:22:54 +0800

    feat: change string attributes to text

--- db/migrate/20220513101329_create_airlines.rb
+++ db/migrate/20220513101329_create_airlines.rb
@@ -1,9 +1,9 @@
 class CreateAirlines < ActiveRecord::Migration[7.0]
   def change
     create_table :airlines do |t|
-      t.string :name
-      t.string :image_url
-      t.string :slug
+      t.text :name
+      t.text :image_url
+      t.text :slug
 
       t.timestamps
     end
